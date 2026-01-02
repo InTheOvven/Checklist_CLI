77d573feae92da4bedbded60a9258ea68781d522
owenb <owen.bacon2006@gmail.com>
2026-01-02 17:11:37 -0500

    Began creating checklist managment tools

--- CheckList/ChecklistManager.cs
+++ CheckList/ChecklistManager.cs
@@ -2,5 +2,38 @@
 
 public class ChecklistManager
 {
-    
+    public void ChecklistCreate()
+    {
+        Console.WriteLine("Enter the name of your new list. This will appear at the top of your checklist: ");
+        string checklistName = Console.ReadLine();
+        
+        StreamWriter newFile = new StreamWriter(checklistName + ".txt");
+        newFile.WriteLine(checklistName);
+        newFile.WriteLine("");
+        newFile.Close();
+    }
+
+    public void ChecklistDelete()
+    {
+        Console.WriteLine("Select a checklist to delete: ");
+        //File.Delete(fileForDeletion);
+        
+        string[] allChecklists = ListChecklists();
+        string selection = Console.ReadLine();
+    }
+
+    private string[] ListChecklists()
+    {
+        int index = 1;
+        string[] files = Directory.GetFiles("", "*.txt");
+        
+        Console.WriteLine("[0] Return to Main Menu");
+        foreach (string file in files)
+        {
+            Console.WriteLine("[" + index + "] " + file);
+            index++;
+        }
+        
+        return files;
+    }
 }
--- CheckList/Program.cs
+++ CheckList/Program.cs
@@ -2,8 +2,14 @@
 
 class Program
 {
+    ChecklistManager manager = new ChecklistManager();
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to Checklist Manager");
+        Console.WriteLine("(1) View/Manage saved checklists");
+        Console.WriteLine("(2) Create a new checklist");
+        Console.WriteLine("(3) Delete a checklist");
+        string selection = Console.ReadLine();
     }
 }
